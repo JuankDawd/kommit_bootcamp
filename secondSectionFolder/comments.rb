53c287aed339c10c937e63dcd5900d3587f13320
Juan Dawd <JuanDawdB@gmail.com>
2021-09-29 18:31:33 -0500

    In this commit is all the code that was explained in the Second Section of the course.

---
+++ secondSectionFolder/comments.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Single line comment
+
+# puts 'Hello world'
+# This prints out "Hello World" to de console
+puts 'Hello world'
+
+puts 'Hello world' # This prints to the console
+
+=begin # rubocop:disable Style/BlockComments
+    In this section of code you have
+    a multi line comment, and as you can see
+    the formatter advices you NOT TO DO IT.
+    FOLLOW THE FORMATTER ADVICES.
+=end
