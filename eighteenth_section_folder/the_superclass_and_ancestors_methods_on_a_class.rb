f310187598df9f980f2dc861dbad4a006d119574
Juan Dawd <JuanDawdB@gmail.com>
2021-10-08 11:15:29 -0500

    Through this commit I display all the knowledge that was impartet to me in the eighteenth section which theme was Classes I of the course.

---
+++ eighteenth_section_folder/the_superclass_and_ancestors_methods_on_a_class.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+p 5.class
+p 5.class.superclass
+p 5.class.superclass.superclass
+p 5.class.superclass.superclass.superclass
+
+puts
+
+p 5.class.ancestors
+
+puts
+
+p 3.14.class
+p 3.14.class.superclass
+p 3.14.class.superclass.superclass
+p 3.14.class.superclass.superclass.superclass
+
+puts
+
+p 3.14.class.ancestors
